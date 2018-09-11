0938293c49923f20dece4bb5a99be2f475f6942a
Patrick Stephansen <patrick@entelect.co.za>
2018-09-11 12:25:18 +0200

    #26 component lab styles interfere with components under test
    This adds some extra padding in the menu for the test case I tried. The styles may need some tweaking.

--- src/frontend/containers/app.ts
+++ src/frontend/containers/app.ts
@@ -1,9 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'component-lab',
-  encapsulation: ViewEncapsulation.None,
   template: `
     <router-outlet></router-outlet>
   `,
@@ -19,8 +18,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
     dl, dt, dd, ol, ul, li,
     fieldset, form, label, legend,
     table, caption, tbody, tfoot, thead, tr, th, td,
-    article, aside, canvas, details, embed, 
-    figure, figcaption, footer, header, hgroup, 
+    article, aside, canvas, details, embed,
+    figure, figcaption, footer, header, hgroup,
     menu, nav, output, ruby, section, summary,
     time, mark, audio, video {
       margin: 0;
@@ -30,7 +29,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
       vertical-align: baseline;
     }
     /* HTML5 display-role reset for older browsers */
-    article, aside, details, figcaption, figure, 
+    article, aside, details, figcaption, figure,
     footer, header, hgroup, menu, nav, section {
       display: block;
     }
